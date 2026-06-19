f6e55c46f718642b77a198b36b8d6903dcc857ab
dxrick <2658526845@qq.com>
2026-06-19 15:01:28 +0800

    fix: logout 后重新创建 ConfigManager，修复闪退
    - config.close() 后不能再使用旧对象
    - 与 on_need_restart 保持一致，close 后重新 new

--- app/main.py
+++ app/main.py
@@ -54,7 +54,9 @@ def main():
                 """修改密码后重新登录"""
                 main_window.close()
                 db.close()
+                nonlocal config
                 config.close()
+                config = ConfigManager()
                 create_login_window()
 
             main_window.logout.connect(on_logout)
